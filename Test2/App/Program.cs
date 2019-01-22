16438c03d7d8339052c5f64667476deeabe7d9e3
Per Samuelsson <per@starcounter.com>
2019-01-22 17:18:58 +0100

    Repro for https://github.com/natemcmaster/DotNetCorePlugins/issues/27

---
+++ Test2/App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace App
+{
+    class NativeMethods
+    {
+        [DllImport("libswipl")]
+		public static extern int PL_is_initialised(IntPtr argc, IntPtr argv);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assure compiler emit reference
+            Console.WriteLine(Starcounter.QueryProcessor.Db.NullString);
+
+            var result = NativeMethods.PL_is_initialised(IntPtr.Zero, IntPtr.Zero);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Test2/Loader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using McMaster.NETCore.Plugins;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appDirectory = Path.Combine(
+                Path.GetDirectoryName(typeof(Program).Assembly.Location),
+                "..",
+                "..",
+                "..",
+                "..",
+                "App",
+                "bin",
+                "Debug",
+                "netcoreapp2.2");
+            var appAssemblyPath = Path.Combine(appDirectory, "App.dll");
+            
+            var loader = PluginLoader.CreateFromAssemblyFile(appAssemblyPath);
+            var appAssembly = loader.LoadDefaultAssembly();
+
+            appAssembly.EntryPoint.Invoke(null, new object[] { Array.Empty<string>() });
+        }
+    }
+}
